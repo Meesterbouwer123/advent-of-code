ba60cf5a93c215274538390960e73ea142480b30
Meesterbouwer123 <meesterbouwer123@gmail.com>
2024-12-02 09:49:10 +0100

    day 1

---
+++ day1/part1.py
@@ -0,0 +1,15 @@
+list1, list2 = [], []
+with open("day1/input.txt") as f:
+    for line in f.readlines():
+        split = line.split()
+        list1.append(int(split[0]))
+        list2.append(int(split[-1]))
+
+list1.sort()
+list2.sort()
+
+sum = 0
+for i in range(len(list1)):
+    sum += abs(list1[i] - list2[i])
+
+print(sum)
---
+++ day1/part2.py
@@ -0,0 +1,12 @@
+list1, list2 = [], []
+with open("day1/input.txt") as f:
+    for line in f.readlines():
+        split = line.split()
+        list1.append(int(split[0]))
+        list2.append(int(split[-1]))
+
+sum = 0
+for item in list1:
+    sum += item * len([x for x in list2 if x == item])
+
+print(sum)
