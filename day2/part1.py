88546f1aaccd7964a91cc3ed3d5cb7edce19b72e
Meesterbouwer123 <meesterbouwer123@gmail.com>
2024-12-02 09:50:51 +0100

    day 2

---
+++ day2/part1.py
@@ -0,0 +1,24 @@
+def issafe(list):
+    last = int(list[0])
+    sign = 0
+    for i in range(1, len(list)):
+        current = int(list[i])
+        if abs(current - last) > 3 or abs(current - last) < 1: 
+            return False
+        currentsign = (current - last) / abs(current - last)
+        if sign != 0 and currentsign != sign: 
+            return False
+        sign = currentsign
+        last = current
+    return True
+
+with open("day2/input.txt") as f:
+    lines = f.readlines()
+
+sum = 0
+for line in lines:
+    split = line.split()
+    safe = issafe(split)
+    if safe: sum += 1
+
+print(sum)
---
+++ day2/part2.py
@@ -0,0 +1,28 @@
+def issafe(list: list[str], skip_index = -1):
+    start = 1 if skip_index == 0 else 0
+    last = int(list[start])
+    sign = 0
+    for i in range(start + 1, len(list)):
+        if i == skip_index: continue
+        current = int(list[i])
+        if abs(current - last) > 3 or abs(current - last) < 1:
+            if skip_index == -1 and (issafe(list, i) or issafe(list, i-1)): return True
+            return False
+        currentsign = 1 if current - last > 0 else -1
+        if sign != 0 and currentsign != sign:
+            if skip_index == -1 and (issafe(list, i) or issafe(list, i-1)): return True
+            return False
+        sign = currentsign
+        last = current
+    return True
+
+with open("day2/input.txt") as f:
+    lines = f.readlines()
+
+sum = 0
+for line in lines:
+    split = line.split()
+    safe = issafe(split) or issafe(split, 0)
+    if safe: sum += 1
+
+print(sum)
