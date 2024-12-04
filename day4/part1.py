58be62382ace60de8440706c48addebe885cba9d
Meesterbouwer123 <meesterbouwer123@gmail.com>
2024-12-04 22:26:52 +0100

    Clean up code

--- day4/part1.py
+++ day4/part1.py
@@ -3,6 +3,7 @@ with open('inputs/day4.txt') as f:
 
 width = len(horizontals[0])
 height = len(horizontals)
+
 verticals = ["".join([horizontals[y][x] for y in range(height) if x < len(horizontals[y])]) for x in range(width)]
 diagonals1 = {}
 diagonals2 = {}
@@ -22,8 +23,7 @@ for x in range(width):
             diagonals2[width*height-x-y] = horizontals[y][x]
 
 res = 0
-for set in [horizontals, verticals, [item for (_, item) in diagonals1.items()], [item for (_, item) in diagonals2.items()]]:
-    res += sum([line.count('XMAS') for line in set])
-    res += sum([line.count('SAMX') for line in set])
+for set in [horizontals, verticals, diagonals1.values(), diagonals2.values()]:
+    res += sum([line.count('XMAS') for line in set]) + sum([line.count('SAMX') for line in set])
 
 print(res)
